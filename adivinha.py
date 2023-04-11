e2c989df35ad03a85733030aa59fcce2badbb0d2
Fernando-Gimenez <fernandohgimenez@gmail.com>
2023-04-11 15:17:26 -0300

    novo 11/04

--- adivinha.py
+++ adivinha.py
@@ -1,17 +1,23 @@
-from random import randint
+    from random import randint
 
-numero = randint(1, 10)
+#numero randomico de 1 a 10
+numero = randint(1, 50)
 
+#numero de tentativas 
 tentativas = 5
+#enquanto um numero maior que 0, chute um numero inteiro (int) se o chute for igual ao nuimero randomico (randit) imprima parabéns!
 while tentativas > 0:
     chute = int(input('numero do chute'))
     diferenca = numero - chute
-    if diferenca == numero:
+    if chute == numero:
         print('parabéns') 
-        break  
+        break 
+    elif diferenca <= 5 and diferenca >= 5:
+        print('vc está perto')    
     else:
-        print('errou')
+        print('errou')   
     tentativas = tentativas -1
+print('fim o numero era:', numero)    
 
 
 
