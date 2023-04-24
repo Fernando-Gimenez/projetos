d01168d073b05ec7db2ead3953c8f30363ca5ea3
Fernando-Gimenez <fernandohgimenez@gmail.com>
2023-04-24 16:45:49 -0300

    atualizado

---
+++ Anotacoes_github.py
@@ -0,0 +1,18 @@
+# Defina uma branch na qual irá trabalhar git checkout dev
+
+# Atualize o seu repositório local git pull origin dev
+
+# Visualize o status do seu arquivo com git status
+# > Você receberá a mensagem de que o arquivo foi modificado
+
+# Adicione para confirmar a alteração git add nome_arquivo
+
+# Confirme que está tudo certo git commit -m “comentário aqui”
+
+# Envie para o repositório remoto git push origin dev
+
+# Mude para a branch desejada git checkout master
+
+# Junte os arquivos da branch master com os arquivos enviados por você para a branch dev git merge dev
+
+# git checkout -b nova_branch origin/master
